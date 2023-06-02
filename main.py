c5496fa707b3e7bcc9610a31673bc7a44d83c948
D'Artagnan Passailaigue <dartagnangilligan@gmail.com>
2023-06-02 14:15:12 -0400

    made the medues work yippie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pygame
-import sys
+import pygame, sys
+from button import Button
 
 def mainmenu():
     pygame.init()
@@ -7,9 +7,6 @@ def mainmenu():
     window_width, window_height = 1536, 864
     window_surface = pygame.display.set_mode((window_width, window_height))
     pygame.display.set_caption("Tournament Bracket Automation (T.B.A.) - Sponsored by Fortnite")
-    WHITE = (255, 255, 255)
-    BLACK = (0, 0, 0)
-    menu_state = "main_menu"
     font1 = pygame.font.SysFont(None, 135)
     font2 = pygame.font.SysFont(None, 50)
     running = True
@@ -20,18 +17,12 @@ def mainmenu():
                 running = False
                 sys.exit()
         mouse_state = pygame.mouse.get_pressed()
-        if menu_state == "main_menu":
-            window_surface.fill((255, 255, 255))  # Clear the screen
-            menu_state = main_menu_screen(window_surface, font1, font2, mouse_state)
-        elif menu_state == "host_menu":
-            host_menu(window_surface, font1)
-            return "host_menu"  # Add this line to return the menu state
+        main_menu_screen(window_surface, font1, font2)
         pygame.display.update()
-        pygame.display.flip()
         clock.tick(60)
     pygame.quit()
     
-def main_menu_screen(surface, font1, font2, mouse):
+def main_menu_screen(surface, font1, font2):
     WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
     surface.fill(WHITE)
@@ -43,18 +34,19 @@ def main_menu_screen(surface, font1, font2, mouse):
     pygame.draw.rect(surface, BLACK, [0, 814, 50, 50], 10)
     pygame.draw.rect(surface, BLACK, [1486, 814, 50, 50], 10)
     # Host rectangle 
-    menu_state = host_button(surface, font2, mouse)  # Assign the menu state returned by the function
-    if menu_state is not None:  # Check if the menu state is not None
-        return menu_state
+    click_host = host_button(surface, font2)  # Assign the menu state returned by the function
     # Join rectangle
-    menu_state = join_button(surface, font2, mouse)  # Assign the menu state returned by the function
-    if menu_state is not None:  # Check if the menu state is not None
-        return menu_state
+    click_join = join_button(surface, font2)  # Assign the menu state returned by the function
+    if click_host:
+        host_menu(surface, font1)
+    elif click_join:
+        join_menu(surface, font1)
 
-def host_button(surface, font, mouse):
+def host_button(surface, font):
     WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
     GREY = (180, 180, 180)
+    click = False
     host_rect = pygame.Rect(500, 250, 500, 100)
     host_surface = pygame.Surface((host_rect.width, host_rect.height))
     host_surface.fill(WHITE)
@@ -75,17 +67,17 @@ def host_button(surface, font, mouse):
         host_tourney_text = font.render("Host Tournament", True, BLACK)
         host_text_centre = host_tourney_text.get_rect(center=host_surface.get_rect().center)
         host_surface.blit(host_tourney_text, host_text_centre)
-        if mouse[0] == 1:  # Check if the left mouse button was pressed down
-            return "host_menu"  # Return the menu state
+        if pygame.mouse.get_pressed()[0]:
+            click = True
 
     surface.blit(host_surface, host_rect)
-    return None
-
+    return click
     
-def join_button(surface, font, mouse):
+def join_button(surface, font):
     WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
     GREY = (180, 180, 180)
+    click = False
     join_rect = pygame.Rect(500, 400, 500, 100)
     join_surface = pygame.Surface((join_rect.width, join_rect.height))
     join_surface.fill(WHITE)
@@ -106,19 +98,49 @@ def join_button(surface, font, mouse):
         join_tourney_text = font.render("Join Tournament", True, BLACK)
         join_text_centre = join_tourney_text.get_rect(center=join_surface.get_rect().center)
         join_surface.blit(join_tourney_text, join_text_centre)
-        if mouse[0] == 1:  # Check if the left mouse button was pressed down
-            return "join_menu"  # Return the menu state
+        if pygame.mouse.get_pressed()[0]:
+            click = True
         
     surface.blit(join_surface, join_rect)
-    return None
+    return click
         
 def host_menu(surface, font):
-    # Implement the host menu here
-    # This is just a placeholder for demonstration
-    surface.fill((255, 0, 0))  # Fill the screen with red color
-    text = font.render("Host Menu", True, (255, 255, 255))
-    text_rect = text.get_rect(center=(surface.get_width() // 2, surface.get_height() // 2))
-    surface.blit(text, text_rect)
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    while True:
+        PLAY_MOUSE_POS = pygame.mouse.get_pos()
+
+        surface.fill(WHITE)
+
+        PLAY_TEXT = font.render("Host Menu", True, BLACK)
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 260))
+        surface.blit(PLAY_TEXT, PLAY_RECT)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
+        
+def join_menu(surface, font):
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    while True:
+        PLAY_MOUSE_POS = pygame.mouse.get_pos()
+
+        surface.fill(WHITE)
+
+        PLAY_TEXT = font.render("Join Menu", True, BLACK)
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 260))
+        surface.blit(PLAY_TEXT, PLAY_RECT)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
     
 # Call the mainmenu function
 mainmenu()
