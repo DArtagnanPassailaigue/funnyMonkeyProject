e173c34378803dfe3142df82d94663dd3d1d8b73
D'Artagnan Passailaigue <dartagnangilligan@gmail.com>
2023-06-12 22:19:22 -0400

    undid that 9my bad)

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pygame, sys
 import math
+
 import random
 import os
 def main_menu(surface, teams):
     while True:
 
         surface.fill(WHITE)
-
         menu_text = font1.render("Tournament Bracket Automation", True, BLACK)
         pygame.draw.rect(surface, BLACK, [0, 0, 1280, 720], 15)
         pygame.draw.rect(surface, BLACK, [0, 0, 50, 50], 10)
@@ -19,7 +19,6 @@ def main_menu(surface, teams):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-
         # Host rectangle 
         click_host = button(surface, 400, 250, 500, 100, "Host Tournament")  # Assign the menu state returned by the function
         # Join rectangle
@@ -63,20 +62,17 @@ def button(surface, xheight, yheight, xlength, ylength, buttontext):
         
     surface.blit(button_surface, button_rect)
     return click
-
 def host_menu(surface, teams):
     teams = teams * 2
     while True:
-
         surface.fill(WHITE)
-
         PLAY_TEXT = font2.render("Host Menu", True, BLACK)
         PLAY_RECT = PLAY_TEXT.get_rect(center=(100, 25))
         surface.blit(PLAY_TEXT, PLAY_RECT)
-        
+
         random.shuffle(player_list)
         draw_bracket(surface, teams, window_width // 2, 0, window_height, window_width // 2 - 100)
-        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -85,14 +81,11 @@ def host_menu(surface, teams):
         click_back = button(surface, 40, 650, 100, 50, "Back")
         if click_back:
             main_menu(surface, teams)
-
         pygame.display.update()    
-
 def draw_bracket(surface, teams, top_x, top_y, height, width):
     if teams > 1:
         level_height = height // int(math.log2(teams))
         half_width = width // 2
-
         pygame.draw.line(surface, BLACK, (top_x, top_y + level_height), (top_x - half_width, top_y + level_height), 2)
         pygame.draw.line(surface, BLACK, (top_x, top_y + level_height), (top_x + half_width, top_y + level_height), 2)
         
@@ -100,6 +93,7 @@ def draw_bracket(surface, teams, top_x, top_y, height, width):
         draw_bracket(surface, teams // 2, top_x + half_width, top_y + level_height, height - level_height, half_width)  # Adjust top_y here
 
         pygame.draw.line(surface, BLACK, (top_x, top_y), (top_x, top_y + level_height//2), 2)
+        button(surface, top_x - 10, top_y + level_height//2, 20, level_height//2 + 2, "PLACEHOLDER")
         if len(player_list) % 2:
                 player_list.append('No entrant')
         for i in range(len(player_list)):
@@ -108,49 +102,42 @@ def draw_bracket(surface, teams, top_x, top_y, height, width):
                 #also, upon entering a name in join menu and going into host menu, the box rapidly switches between the name and "no entrant"
     else:
         pygame.draw.line(surface, BLACK, (top_x, top_y), (top_x, top_y + height//2), 2)
+        button(surface, top_x - 10, top_y + height//2, 20, height//2 + 2, "PLACEHOLDER")
         button(surface, top_x - 10, top_y + height//2, 100, height//2 + 2, "No Participants ")
 
 def next_power_of_2(n):
     return 2 ** math.ceil(math.log2(n))
-
 def join_menu(surface, text, teams):
     text_editing = False
     text_rect_height = 325
     boxnum = 0
-
     while True:
         surface.fill(WHITE)
         PLAY_TEXT = font1.render("Join Menu", True, BLACK)
         PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 100))
         surface.blit(PLAY_TEXT, PLAY_RECT)
-            
         text_rect = pygame.Rect(400, text_rect_height, 500, 50)
         text_surface = pygame.Surface((text_rect.width, text_rect.height))
         text_surface.fill(WHITE)
         pygame.draw.rect(text_surface, BLACK, text_surface.get_rect(), 5)
-
         mouse_pos = pygame.mouse.get_pos()
         if text_rect.collidepoint(mouse_pos):
             text_surface.fill(GREY)
             pygame.draw.rect(text_surface, BLACK, text_surface.get_rect(), 5)
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if text_rect.collidepoint(event.pos):
                     text_editing = True
                 elif not text_rect.collidepoint(event.pos):
                     text_editing = False
-
             if event.type == pygame.KEYDOWN and text_editing:
                 if event.key == pygame.K_BACKSPACE:
                     text = text[:-1]
                 else:
                     text += event.unicode
-
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
                     join_input_return(text)
@@ -162,25 +149,38 @@ def join_menu(surface, text, teams):
                     pygame.display.update()
                     pygame.time.delay(2000)  # Delay in milliseconds
                     surface.fill(WHITE)
-
+                    #upon revamping the button functions, add a back button and add player button here
         click_back = button(surface, 40, 650, 100, 50, "Back")
         click_add = button(surface, 500, 200, 300, 75, "+ Add Player")
         if click_back:
             main_menu(surface, teams)
         elif click_add:
-            add_player_clicks += 1
-            addplayer(surface, text_rect_height, boxnum)  # Pass player_boxes as a parameter
-
+            #text_rect_height += 25
+            #boxnum += 1
+            addplayer(surface, text_rect_height, boxnum)
         input_text = font2.render(text, True, BLACK)
         input_rect = input_text.get_rect(center=text_rect.center)
         surface.blit(text_surface, text_rect)
         surface.blit(input_text, input_rect)
-
         pygame.display.update()
+                    
+def addplayer(surface, height, box_num):
+    # come back to this later
+    pass
+    '''
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    GREY = (180, 180, 180)
+    extra_boxes = []
+    while True:
+        box_num
+    '''
 
-def textbox(surface, height)
-        
+
 def join_input_return(text):
+    playerX = text
+    print(playerX)
+    # ruban's player input code will go here
     player_list.append(text)
     fileName = folder + "players.csv"
     file = open(fileName,"a")    
@@ -202,7 +202,6 @@ font1 = pygame.font.SysFont(None, 111)
 font2 = pygame.font.SysFont(None, 50)
 font3 = pygame.font.SysFont(None, 30)
 running = True
-add_player_clicks = 0
 text = ""
 player_list = []
 teams = 0
